e4c55ede195f6c8de6f8f5e9d226e2effb484904
Shubham Madankar <shubham.madankar@nium.com>
2024-09-18 18:16:29 +0530

    Added storybook

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,8 +3,6 @@ import React, { FC, ReactNode } from 'react';
 interface Props {
   children: ReactNode | ReactNode[];
 }
-const Button: FC<Props> = ({ children }) => {
+export const Button: FC<Props> = ({ children }) => {
   return <div>{children}</div>;
 };
-
-export default Button;
